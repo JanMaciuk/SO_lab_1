4c8e53cd07e2a81badf0a9f861548c4b55a39ac9
JanMaciuk <38810823+JanMaciuk@users.noreply.github.com>
2023-03-09 16:42:26 +0100

    Initial commit

---
+++ src/Proces.java
@@ -0,0 +1,8 @@
+public class Proces {
+    protected int length;  //Całkowita długość
+    protected int timeLeft; //Ile czasu zostało do zakończenia
+    protected int waitingTime; //Jak długo czekał niewykonywany
+    protected int arrivalTime;  //Kiedy zaczniemy go wykonywać
+
+
+}
